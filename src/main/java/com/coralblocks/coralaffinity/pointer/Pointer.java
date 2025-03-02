2c5696925df7e93fbc3d715083ab1504d9b5672d
Sergio Oliveira <sergio.oliveira@coralblocks.com>
2025-03-02 07:10:59 -0500

    Adding all pointers to list

--- src/main/java/com/coralblocks/coralaffinity/pointer/Pointer.java
+++ src/main/java/com/coralblocks/coralaffinity/pointer/Pointer.java
@@ -1,5 +1,8 @@
 package com.coralblocks.coralaffinity.pointer;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.coralblocks.coralaffinity.CpuMask;
 import com.sun.jna.ptr.ByReference;
 
@@ -15,5 +18,42 @@ public abstract class Pointer extends ByReference implements CpuMask {
 	@Override
 	public final int getSizeInBytes() {
 		return sizeInBytes;
+	}
+	
+	public static final List<Pointer> ALL = new ArrayList<Pointer>(32);
+	
+	static {
+		ALL.add(new _8BitsPointer());
+		ALL.add(new _16BitsPointer());
+		ALL.add(new _24BitsPointer());
+		ALL.add(new _32BitsPointer());
+		ALL.add(new _40BitsPointer());
+		ALL.add(new _48BitsPointer());
+		ALL.add(new _56BitsPointer());
+		ALL.add(new _64BitsPointer());
+		ALL.add(new _72BitsPointer());
+		ALL.add(new _80BitsPointer());
+		ALL.add(new _88BitsPointer());
+		ALL.add(new _96BitsPointer());
+		ALL.add(new _104BitsPointer());
+		ALL.add(new _112BitsPointer());
+		ALL.add(new _120BitsPointer());
+		ALL.add(new _128BitsPointer());
+		ALL.add(new _136BitsPointer());
+		ALL.add(new _144BitsPointer());
+		ALL.add(new _152BitsPointer());
+		ALL.add(new _160BitsPointer());
+		ALL.add(new _168BitsPointer());
+		ALL.add(new _176BitsPointer());
+		ALL.add(new _184BitsPointer());
+		ALL.add(new _192BitsPointer());
+		ALL.add(new _200BitsPointer());
+		ALL.add(new _208BitsPointer());
+		ALL.add(new _216BitsPointer());
+		ALL.add(new _224BitsPointer());
+		ALL.add(new _232BitsPointer());
+		ALL.add(new _240BitsPointer());
+		ALL.add(new _248BitsPointer());
+		ALL.add(new _256BitsPointer());
 	}
 }
