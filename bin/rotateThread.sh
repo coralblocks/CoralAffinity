76560e3a0e4953db8759d61ef61c580a905fddd2
Sergio Oliveira <sergio.oliveira@coralblocks.com>
2025-03-07 05:26:10 -0500

    Change defalut

--- bin/rotateThread.sh
+++ bin/rotateThread.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ISOLATED=${1:-true}
+ISOLATED=${1:-false}
 VERBOSE=${2:-false}
 
 CMD="java -DcoralAffinityVerbose=$VERBOSE -cp target/coralaffinity-all.jar com.coralblocks.coralaffinity.sample.RotateThread $ISOLATED"
